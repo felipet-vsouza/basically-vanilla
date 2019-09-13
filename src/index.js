99fe7e2449e6901b77cf35ff90776497dc234530
Felipe Thomas Vargas de Souza <felipe.souza@cwi.com.br>
2019-09-13 14:58:51 -0300

    Formats all files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import { App } from './app.js';
-import { Authors } from './app/authors/authors.jsx';
-import { Documentation } from './app/documentation/documentation.jsx';
-import { Homepage } from './app/homepage/homepage.jsx';
-import { appContainer } from './basic/helper/dom.js';
+import {
+    App
+} from './app.js';
+import {
+    Authors
+} from './app/authors/authors.jsx';
+import {
+    Documentation
+} from './app/documentation/documentation.jsx';
+import {
+    Homepage
+} from './app/homepage/homepage.jsx';
+import {
+    appContainer
+} from './basic/helper/dom.js';
 
 function renderApproach(Component) {
     return () => appContainer().innerHTML = Component();
@@ -10,11 +20,22 @@ function renderApproach(Component) {
 
 document.addEventListener('DOMContentLoaded', () => {
     new App(
-        [
-            { name: 'Homepage', href: '/', component: Homepage },
-            { name: 'Documentation', href: '/docs/', component: Documentation },
-            { name: 'About the authors', href: '/authors/', component: Authors }
-        ]
-    )
+            [{
+                    name: 'Homepage',
+                    href: '/',
+                    component: Homepage
+                },
+                {
+                    name: 'Documentation',
+                    href: '/docs/',
+                    component: Documentation
+                },
+                {
+                    name: 'About the authors',
+                    href: '/authors/',
+                    component: Authors
+                }
+            ]
+        )
         .initialize(renderApproach);
 });
